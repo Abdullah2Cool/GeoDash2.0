39a838684dd81f294b7fb8c0674ec672ee4ce79d
Abdullah2Cool <abdullahbinasad14@gmail.com>
2016-12-13 14:27:04 -0500

    Initial Commit.

---
+++ core/src/game/release2/GamGeoDash.java
@@ -0,0 +1,29 @@
+package game.release2;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public class GamGeoDash extends Game {
+
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void setScreen(Screen screen) {
+		super.setScreen(screen);
+	}
+
+	@Override
+	public void render () {
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		super.dispose();
+	}
+}
---
+++ desktop/src/game/release2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.release2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.release2.GamGeoDash;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GamGeoDash(), config);
+	}
+}
